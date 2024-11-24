8dbce8de7d1c6fe3d51e789dd2981a4c98204d9a
pisquareforce <pisquareforce@gmail.com>
2024-11-24 17:54:51 +0000

    Update Logger.sh

--- exercises/class18/Logger.sh
+++ exercises/class18/Logger.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
 
+# Turn it executable file: chmod +x Logger.sh
+
+# References: 
+#    https://stackoverflow.com/questions/1401482/yyyy-mm-dd-format-date-in-shell-script
+#    https://www.cyberciti.biz/faq/unix-linux-getting-current-date-in-bash-ksh-shell-script/
+#    https://serverfault.com/questions/196734/bash-difference-between-and-operator
+
+#
+# 1. Current date time
+#
+c_datetime=""
+
+#
+# 2. Get the user
+#
+user=$USER
+
+#
+# 3. Get the current time
+#
+c_datetime=$(date "+%Y-%m-%d %H:%M:%S")
+
+
+#
+# 4. Message
+#
+message="The software executed successfully!"
+
+
+#
+# 5.Append the log to the file Activity.log
+#
+
+echo "[$c_datetime] User: $user - $message" >> Activity.log
