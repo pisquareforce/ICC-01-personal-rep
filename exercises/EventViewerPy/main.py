27c16edd8e039c7c6e15c39afef304703034c542
pisquareforce <pisquareforce@gmail.com>
2024-11-28 17:31:55 +0000

    Create main.py

---
+++ exercises/EventViewerPy/main.py
@@ -0,0 +1,17 @@
+import win32evtlog
+import win32evtlogutil
+import win32security
+import win32con
+import winerror
+import time
+import re
+import string
+import sys
+import traceback
+
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
